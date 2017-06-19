0a0642df4e6f17312afa7fb41b0cc736fdee1974
Morvan Zhou <mz@email.com>
2017-06-20 01:16:04 +0800

    [20170620011554479]

--- app/src/main/java/com/androidstudio/myapplication/MainActivity.java
+++ app/src/main/java/com/androidstudio/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.androidstudio.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.androidstudio.myapplication.textutils.TextUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTextViewText();
+        for(int counter = 0; counter < 10; counter++){
+            Log.i("MainActivity","Value:" + counter);
+        }
+    }
+
+    private void setTextViewText() {
+        TextUtil textUtil = new TextUtil();
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("abc");
     }
 }
---
+++ app/src/main/java/com/androidstudio/myapplication/textutils/TextUtil.java
@@ -0,0 +1,8 @@
+package com.androidstudio.myapplication.textutils;
+
+/**
+ * Created by Win10 on 2017/5/26.
+ */
+
+public class TextUtil {
+}
